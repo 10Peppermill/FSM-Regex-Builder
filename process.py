52cd0a2a160562b8c8da94822142886b4a1bf161
10Peppermill <cohen.jon@husky.neu.edu>
2022-07-07 20:09:41 -0400

    focusing on building the transition function

--- process.py
+++ process.py
@@ -1,38 +1,62 @@
+from inspect import Attribute
 import sys
 from typing import Dict
 from greenery import fsm, lego
+import re
+import numpy
 
-UC = set(hex(e) for e in range(sys.maxunicode))
-UC_NUMBERS = set(chr(e) for e in range(48,57,1))
-UC_UPPERS = set(chr(e) for e in range(65,90,1))
-UC_LOWERS = set(chr(e) for e in range(97,122,1))
-#UC = set(range(sys.maxunicode))
-#UC_NUMBERS = set(range(48,57,1))
-#UC_UPPERS = set(range(65,90,1))
-#UC_LOWERS = set(range(97,122,1))
+UC = set(chr(e) for e in range(48,(2**7)-1,1))
+UC_NUMBERS = set(chr(e) for e in range(48,58,1))
+UC_UPPERS = set(chr(e) for e in range(65,91,1))
+UC_LOWERS = set(chr(e) for e in range(97,123,1))
 EMPTY_SET = set()
 
-
+transitionFunction = dict()
+state1 = dict()
+state2 = dict()
 Q1, Q2 = range(2)
+localLanguage = EMPTY_SET.union(UC_NUMBERS, UC_LOWERS, UC_UPPERS)
 
 a_Q = {Q1,Q2}
-a_Sigma = EMPTY_SET.union(UC_NUMBERS, UC_LOWERS, UC_UPPERS)
+a_Sigma = UC
 a_q = Q1
-a_F = {Q2}
+a_F = {Q1}
 
+diffrence = a_Sigma.difference(localLanguage)
+
+for e in UC_NUMBERS:
+    state1.update({e : Q2})
+for e in UC_UPPERS:
+    state1.update({e : Q1})
+for e in UC_LOWERS:
+    state1.update({e : Q1})
+for e in diffrence:
+    state1.update({e : Q2})
+
+for e in UC_NUMBERS:
+    state2.update({e : Q2})
+for e in UC_UPPERS:
+    state2.update({e : Q2})
+for e in UC_LOWERS:
+    state2.update({e : Q2})
+for e in diffrence:
+    state2.update({e : Q2})
+
+transitionFunction.update({Q1 : state1})
+transitionFunction.update({Q2 : state2})
+
+# create the FSM
 finiteAutomata = fsm.fsm(
     states      = a_Q,
     alphabet    = a_Sigma,
     initial     = a_q,
     finals      = a_F,
-    map         = {
-         Q1 : {str(UC_NUMBERS): Q2, str(UC_LOWERS): Q1, str(UC_UPPERS): Q1},
-         Q2 : {str(UC_NUMBERS): Q2, str(UC_LOWERS): Q2, str(UC_UPPERS): Q2}
-    } 
+    map         = transitionFunction 
 )
-# create the FSM
-
+print(finiteAutomata)
 
 # convert it to regex
 rex = lego.from_fsm(finiteAutomata)
 print(rex)
+
+print("BYE")
